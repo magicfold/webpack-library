d340b59f946d640bb2c7ef1f57bcd6f0862803bd
peter <peter@soez.io>
2019-08-01 14:23:23 +0800

    暴露library

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,8 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'webpack-numbers.js'
+        filename: 'webpack-numbers.js',
+        library: 'webpackNumbers'   //暴露library（将该library bundle暴露为名为 webpackNumbers 的全局变量）
     },
     externals: { // 外部化lodash（lodash不会被打包进代码。用户在使用本webpack-library包时，需要在自己的环境安装lodash）
         lodash: {
