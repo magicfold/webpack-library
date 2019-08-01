bd5bf3704b71e55ae79db2c430b1d5042643bec4
peter <peter@soez.io>
2019-08-01 14:22:37 +0800

    外部化lodash，打包时不将lodash打包进代码

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,13 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'webpack-numbers.js'
+    },
+    externals: { // 外部化lodash（lodash不会被打包进代码。用户在使用本webpack-library包时，需要在自己的环境安装lodash）
+        lodash: {
+            commonjs: 'lodash',
+            commonjs2: 'lodash',
+            amd: 'lodash',
+            root: '_'
+        }
     }
 };
